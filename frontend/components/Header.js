d2507a473b8833c38db8841f04c4b5442354c525
Adrien NEYRON <adrien.neyron@gmail.com>
2023-10-02 20:12:44 +0200

    header transparency & link

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,37 +1,29 @@
-import styles from "../styles/Header.module.css"
+import styles from "../styles/Header.module.css";
 import Link from "next/link";
 import React from "react";
 
 function Header() {
-    return(
-        <div className={styles.menu}>
-          <div className={styles.contentMenu}>
-            <Link href="/" className={styles.section}>
-              HOME
-            </Link>
-          </div>
-          <div className={styles.contentMenu}>
-            <Link href="/#section1" className={styles.section}>
-              QUESTION
-            </Link>
-          </div>
-          <div className={styles.contentMenu}>
-            <Link href="/#section2" className={styles.section}>
-              TEMOIGNAGES
-            </Link>
-          </div>
-          <div className={styles.contentMenu}>
-            <Link href="/#section3" className={styles.section}>
-              ABOUT ME
-            </Link>
-          </div>
-          <div className={styles.contentMenu}>
-            <Link href="/#section4" className={styles.section}>
-              CONTACT
-            </Link>
-          </div>
-        </div>
-    );
+  return (
+    <div className={styles.menu}>
+      <div className={styles.contentMenu}>
+        <Link href="/" className={styles.section}>
+          HOME
+        </Link>
+      </div>
+      <Link href="/#section1" className={styles.section}>
+        <div className={styles.contentMenu}>QUESTION</div>
+      </Link>
+      <Link href="/#section2" className={styles.section}>
+        <div className={styles.contentMenu}>TEMOIGNAGES</div>
+      </Link>
+      <Link href="/#section3" className={styles.section}>
+        <div className={styles.contentMenu}>ABOUT ME</div>
+      </Link>
+      <Link href="/#section4" className={styles.section}>
+        <div className={styles.contentMenu}>CONTACT</div>
+      </Link>
+    </div>
+  );
 }
 
 export default Header;
